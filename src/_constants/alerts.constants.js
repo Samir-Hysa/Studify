7b74031a75f2b2fc236650a5811d05c1d925a6da
samir.hysa <samir.hysa.work@gmail.com>
2021-05-17 20:23:21 +0200

    reset project

--- src/_constants/alerts.constants.js
+++
@@ -1,5 +0,0 @@
-export const alertConstants = {
-  SUCCESS: "ALERTS_SUCCESS",
-  ERROR: "ALERT_ERROR",
-  CLEAR: "ALERT_CLEAR",
-};
--- src/components/Landing/LandingStyles.js
+++
@@ -1,7 +0,0 @@
-import { makeStyles } from "@material-ui/core";
-
-const LandingStyle = makeStyles((theme) => ({
-  root: {},
-}));
-
-export default LandingStyle;
